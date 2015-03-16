cc486e6e1ab2138757b2cb887951e9fa1abc72ec
Robin <me@invokr.org>
2015-03-16 17:09:55 +0000

    Added additional varint_fast function as well as new tests for it

--- src/alice2/util/varint.hpp
+++ src/alice2/util/varint.hpp
@@ -59,37 +59,37 @@ namespace alice {
         };
 
         template<>
-        char varIntBuffer<const char*&>::get() {
+        inline char varIntBuffer<const char*&>::get() {
             assert(mPos != mSize); // Trying to read past buffer boundaries
             return mBuffer[mPos++];
         }
 
         template<>
-        char varIntBuffer<char*&>::get() {
+        inline char varIntBuffer<char*&>::get() {
             assert(mPos != mSize); // Trying to read past buffer boundaries
             return mBuffer[mPos++];
         }
 
         template<>
-        char varIntBuffer<const unsigned char*&>::get() {
+        inline char varIntBuffer<const unsigned char*&>::get() {
             assert(mPos != mSize); // Trying to read past buffer boundaries
             return mBuffer[mPos++];
         }
 
         template<>
-        char varIntBuffer <unsigned char*&>::get() {
+        inline char varIntBuffer <unsigned char*&>::get() {
             assert(mPos != mSize); // Trying to read past buffer boundaries
             return mBuffer[mPos++];
         }
 
         template<>
-        char varIntBuffer<std::istream&>::get() {
+        inline char varIntBuffer<std::istream&>::get() {
             assert(mBuffer.good()); // Trying to read past buffer boundaries
             return mBuffer.get();
         }
 
         template<>
-        char varIntBuffer<std::string&>::get() {
+        inline char varIntBuffer<std::string&>::get() {
             assert(mPos != mBuffer.size()); // Trying to read past buffer boundaries
             return mBuffer[mPos++];
         }
@@ -116,9 +116,67 @@ namespace alice {
         return result;
     }
 
+    /** Read a varint from a buffer, fast version. */
+    inline uint8_t* readVarUInt32_fast(uint8_t* buffer, uint32_t& value) {
+        uint8_t* ptr = buffer; // avoids indirection, indicates bytes read
+        uint32_t b;
+        uint32_t result = 0;
+
+        assert(ptr);
+
+        b = *(ptr++); result  = b      ; if (!(b & 0x80)) goto done;
+        result -= 0x80;
+        b = *(ptr++); result += b <<  7; if (!(b & 0x80)) goto done;
+        result -= 0x80 << 7;
+        b = *(ptr++); result += b << 14; if (!(b & 0x80)) goto done;
+        result -= 0x80 << 14;
+        b = *(ptr++); result += b << 21; if (!(b & 0x80)) goto done;
+        result -= 0x80 << 21;
+        b = *(ptr++); result += b << 28; if (!(b & 0x80)) goto done;
+
+        // More than 5 bytes read, corrupt
+        return nullptr;
+
+        done:
+            value = result;
+            return ptr;
+    }
+
+    /** Writes a varint to the given buffer, requires size > 4 */
+    inline uint8_t* writeVarUInt32(uint8_t* buffer, uint32_t value) {
+        assert(buffer);
+
+        buffer[0] = static_cast<uint8_t>(value | 0x80);
+        if (value >= (1 << 7)) {
+            buffer[1] = static_cast<uint8_t>((value >>  7) | 0x80);
+            if (value >= (1 << 14)) {
+                buffer[2] = static_cast<uint8_t>((value >> 14) | 0x80);
+                if (value >= (1 << 21)) {
+                    buffer[3] = static_cast<uint8_t>((value >> 21) | 0x80);
+                    if (value >= (1 << 28)) {
+                        buffer[4] = static_cast<uint8_t>(value >> 28);
+                        return buffer + 5;
+                    } else {
+                        buffer[3] &= 0x7F;
+                        return buffer + 4;
+                    }
+                } else {
+                    buffer[2] &= 0x7F;
+                    return buffer + 3;
+                }
+            } else {
+                buffer[1] &= 0x7F;
+                return buffer + 2;
+            }
+        } else {
+            buffer[0] &= 0x7F;
+            return buffer + 1;
+        }
+    }
+
     /** Reads an uint64_t, encoded by protobuf */
     template <typename T>
-    uint64_t readVarUInt64(T&& t, uint8_t& read, std::size_t size = 0, std::size_t position = 0) {
+    inline uint64_t readVarUInt64(T&& t, uint8_t& read, std::size_t size = 0, std::size_t position = 0) {
         uint64_t result = 0; // result
         char tmp;            // holds last read value
         read = 0;            // make sure read is zeroed
@@ -135,6 +193,42 @@ namespace alice {
         } while (tmp & 0x80);
 
         return result;
+    }
+
+    /** Read a varint from a buffer, fast version. */
+    inline uint8_t* readVarUInt64_fast(uint8_t* buffer, uint64_t& value) {
+        uint8_t* ptr = buffer; // avoids indirection, indicates bytes read
+        uint32_t b;
+        uint32_t part0 = 0, part1 = 0, part2 = 0;
+
+        b = *(ptr++); part0  = b      ; if (!(b & 0x80)) goto done;
+        part0 -= 0x80;
+        b = *(ptr++); part0 += b <<  7; if (!(b & 0x80)) goto done;
+        part0 -= 0x80 << 7;
+        b = *(ptr++); part0 += b << 14; if (!(b & 0x80)) goto done;
+        part0 -= 0x80 << 14;
+        b = *(ptr++); part0 += b << 21; if (!(b & 0x80)) goto done;
+        part0 -= 0x80 << 21;
+        b = *(ptr++); part1  = b      ; if (!(b & 0x80)) goto done;
+        part1 -= 0x80;
+        b = *(ptr++); part1 += b <<  7; if (!(b & 0x80)) goto done;
+        part1 -= 0x80 << 7;
+        b = *(ptr++); part1 += b << 14; if (!(b & 0x80)) goto done;
+        part1 -= 0x80 << 14;
+        b = *(ptr++); part1 += b << 21; if (!(b & 0x80)) goto done;
+        part1 -= 0x80 << 21;
+        b = *(ptr++); part2  = b      ; if (!(b & 0x80)) goto done;
+        part2 -= 0x80;
+        b = *(ptr++); part2 += b <<  7; if (!(b & 0x80)) goto done;
+
+        // More than 10 bytes read, corrupt
+        return nullptr;
+
+        done:
+            value = (static_cast<uint64_t>(part0)) |
+             (static_cast<uint64_t>(part1) << 28) |
+             (static_cast<uint64_t>(part2) << 56);
+            return ptr;
     }
 }
 
--- test/alice2/util/varint.cpp
+++ test/alice2/util/varint.cpp
@@ -38,6 +38,29 @@ TEST_CASE( "varint", "[util/varint.hpp]" ) {
     // Try reading from a std::string
     std::string av2 = {(char)0x8E, 0x02};
 
+    std::string av3 = {(char)0x96, 0x01};
+    REQUIRE(readVarUInt64(av3, read) == 150);
+
+    // Try the 64 version
     REQUIRE(readVarUInt64(av2, read) == 270);
     REQUIRE(read == 2);
+
+    // Write a varint
+    uint8_t cWrite[3];
+    writeVarUInt32(&cWrite[0], 86942);
+
+    REQUIRE(cWrite[0] == 0x9E);
+    REQUIRE(cWrite[1] == 0xA7);
+    REQUIRE(cWrite[2] == 0x05);
+
+    // Try the fast 32 bit version
+    uint32_t res = 0;
+    uint8_t *rBuf = readVarUInt32_fast((uint8_t*)c2, res);
+    REQUIRE(res == 86942);
+    REQUIRE((rBuf - (uint8_t*)c2) == 3);
+
+    // Try the fast 64 bit version
+    uint64_t res64 = 0;
+    readVarUInt64_fast((uint8_t*)c2, res64); 
+    REQUIRE(res64 == 86942);
 }
